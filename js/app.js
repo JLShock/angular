cb52e37e2429ca668a774ebbb839b5c6798eee47
John Shockey <Shockey@John-Shockeys-MacBook-Pro.local>
2016-01-05 12:22:41 -0800

    add multiple products

--- js/app.js
+++ js/app.js
@@ -3,15 +3,34 @@
 	var app = angular.module('store', [ ]);
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = {
-		name: 'Skywalker',
-		price: 5.95,
-		description: "The Skywalker gem's rare green hue is known to have special powers.",
-		canPurchase: true,
-		soldOut: false
-	};
+	var gems = [
+		{
+			name: 'Skywalker',
+			price: 5.95,
+			color: 'Green',
+			description: "The Skywalker gem is known to have special tactical powers.",
+			canPurchase: true,
+			soldOut: false,
+		},
+		{
+			name: 'Vader',
+			price: 9.95,
+			color: 'Red',
+			description: "The Vader gem is known to have special strength powers.",
+			canPurchase: true,
+			soldOut: false,
+		},
+		{
+			name: 'Obi',
+			price: 7.95,
+			color: 'Blue',
+			description: "The Obi gem is known to have special mind reading powers.",
+			canPurchase: true,
+			soldOut: false,
+		},
+	];
 
 })();
