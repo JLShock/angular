d9f107cfe06098864cf57d1ed3aebd187c81bdb4
John Shockey <Shockey@John-Shockeys-MacBook-Pro.local>
2016-01-05 12:13:30 -0800

    add controller, product,  directives

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('store', [ ]);
+(function(){
+
+	var app = angular.module('store', [ ]);
+
+	app.controller('StoreController', function(){
+		this.product = gem;
+	});
+
+	var gem = {
+		name: 'Skywalker',
+		price: 5.95,
+		description: "The Skywalker gem's rare green hue is known to have special powers.",
+		canPurchase: true,
+		soldOut: false
+	};
+
+})();
